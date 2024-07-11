904d89f3c746e6228bd8800b06b99775700ba29b
Luis Navarro <luis_winche@hotmail.com>
2024-07-11 15:17:32 -0400

    c3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import vercel from '@astrojs/vercel/serverless';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: vercel(),
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: netlify()
 });
